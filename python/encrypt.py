217c87a665e0d05abac81e4aafc8b638c4d9eaf0
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-06-04 16:39:44 +0200

    Create substitution cypher encryption program

---
+++ python/encrypt.py
@@ -0,0 +1,54 @@
+﻿# ===================================================
+#                     DESCRIPTION
+# ===================================================
+
+'''
+Python substitution cypher encryption program
+'''
+
+# ===================================================
+#                       IMPORTS
+# ===================================================
+
+import random
+import string
+
+# ===================================================
+#                     MAIN PROGRAM
+# ===================================================
+
+chars = " " + string.punctuation + string.digits + string.ascii_letters
+chars = list(chars)
+key = chars.copy()
+random.shuffle(key)
+
+# print(f"chars: {chars}")
+# print(f"key: {key}")
+
+# ===================================================
+#                       ENCRYPT
+# ===================================================
+
+plain_text = input("Enter a message to encrypt: ")
+cipher_text = ""
+
+for letter in plain_text:
+    index = chars.index(letter)
+    cipher_text += key[index]
+
+print(f"original message: {plain_text}")
+print(f"cipher message: {cipher_text}")
+
+# ===================================================
+#                       DECRYPT
+# ===================================================
+
+cipher_text = input("Enter a message to decrypt: ")
+plain_text = ""
+
+for letter in cipher_text:
+    index = key.index(letter)
+    plain_text += chars[index]
+
+print(f"encrypted message: {cipher_text}")
+print(f"original message: {plain_text}")
