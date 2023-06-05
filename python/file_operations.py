22412cbb64470f2ee13d30a5c5ffd7851db190fc
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-06-05 12:46:20 +0200

    Create script with basic file operations
    Add file/directory detection

---
+++ python/file_operations.py
@@ -0,0 +1,37 @@
+﻿# ===================================================
+#                     DESCRIPTION
+# ===================================================
+
+'''
+Python program to handle file operations
+'''
+
+# ===================================================
+#                       IMPORTS
+# ===================================================
+
+import os
+
+# ===================================================
+#                     DETECT FILE
+# ===================================================
+
+
+def detect_file(path):
+    if os.path.exists(path):
+        print("This location exists!")
+        if os.path.isfile(path):
+            print("This is a file.")
+        elif os.path.isdir(path):
+            print("This is a directory.")
+    else:
+        print("This locations doesn't exist!")
+
+# ===================================================
+#                     MAIN FUNCTION
+# ===================================================
+
+
+path = input("Provide a path to the file: ")
+
+detect_file(path)
