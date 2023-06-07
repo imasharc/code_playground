216134a425e5577dcb3052054efef5d7ab8346d7
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-06-07 10:08:15 +0200

    Instance variables vs class variables

--- python/oop/main.py
+++ python/oop/main.py
@@ -3,10 +3,14 @@
 car_1 = Car("Chevy", "Corvette", 2021, "blue")
 car_2 = Car("Ford", "Mustang", 2022, "red")
 
-print(car_2.make)
-print(car_2.model)
-print(car_2.year)
-print(car_2.color)
+car_1.wheels = 2
+
+print(car_1.wheels)
+print(car_2.wheels)
 
 car_2.drive()
 car_2.stop()
+
+Car.wheels = 2
+
+print(Car.wheels)
