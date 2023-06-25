a38e222cd3f6f8eca57c5f00408a133458ca6b9d
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-06-25 17:16:05 +0200

    Add interaction logic for MainWindow.xaml
    Add image controls to the game grid

--- c_sharp/Snake/Snake/MainWindow.xaml.cs
+++ c_sharp/Snake/Snake/MainWindow.xaml.cs
@@ -20,9 +20,42 @@ namespace Snake
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly int rows = 15, columns = 15;
+        // This array will make it easy to access the image for a given position in the grid
+        private readonly Image[,] gridImages;
+
         public MainWindow()
         {
             InitializeComponent();
+            gridImages = SetupGrid();
+        }
+
+        // It will add the required image controls to the GameGrid and return them in a 2D array for easy access 
+        private Image[,] SetupGrid()
+        {
+            Image[,] images = new Image[rows, columns];
+            GameGrid.Rows = rows;
+            GameGrid.Columns = columns;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int column = 0; column < columns; column++)
+                {
+                    // for each of them we create a new image
+                    Image image = new Image
+                    {
+                        // Initialy we want source to be the Empty image asset
+                        Source = Images.Empty
+                    };
+
+                    // We store this image in the 2D array
+                    images[row, column] = image;
+                    // and add it as a child of the GameGrid
+                    GameGrid.Children.Add(image);
+                }
+            }
+
+            return images;
         }
     }
 }
