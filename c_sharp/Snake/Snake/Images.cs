ab06a090043f999786e98883cdf3de5630ca39a6
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-06-25 16:28:26 +0200

    Add container for all our image assets

---
+++ c_sharp/Snake/Snake/Images.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Snake
+{
+    // This class will be a container for all our image assets
+    public static class Images
+    {
+        public readonly static ImageSource Empty = LoadImage("Empty.png");
+        public readonly static ImageSource Body = LoadImage("Body.png");
+        public readonly static ImageSource Head = LoadImage("Head.png");
+        public readonly static ImageSource Food = LoadImage("Food.png");
+        public readonly static ImageSource DeadBody = LoadImage("DeadBody.png");
+        public readonly static ImageSource DeadHead = LoadImage("DeadHead.png");
+
+        // Loads the image with the given fileName and returns it as an image source
+        private static ImageSource LoadImage(string fileName)
+        {
+            return new BitmapImage(new Uri($"Assets/{fileName}", UriKind.Relative));
+        }
+    }
+}
