7fb69c7a2ffa61b51826528a096d672fedb330cd
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-06-26 11:15:17 +0200

    Fix bug - moving by too much positions for the Right direction

--- c_sharp/Snake/Snake/Direction.cs
+++ c_sharp/Snake/Snake/Direction.cs
@@ -6,7 +6,7 @@ namespace Snake
     public class Direction
     {
         public readonly static Direction Left = new Direction(0, -1);
-        public readonly static Direction Right = new Direction(0, 11);
+        public readonly static Direction Right = new Direction(0, 1);
         public readonly static Direction Up = new Direction(-1, 0);
         public readonly static Direction Down = new Direction(1, 0);
 
