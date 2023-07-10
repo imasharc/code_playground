74b9f4c349f1ff635c0faa74fc87569ab402bbe1
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-07-10 18:45:41 +0200

    Further clarify the time argument in the animate(time)

--- javascript/three_js/main.js
+++ javascript/three_js/main.js
@@ -26,8 +26,9 @@ const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(box);
 
-// rotate the cube by 0.01 radians on the X and Y axes every second
 // continuously update the rotation of the box object based on the current time (timestamp)
+// By dividing the time by 1000, it converts the time from milliseconds to seconds
+// The resulting value is then assigned to the rotation.y and rotation.x properties of the box, causing it to rotate over time.
 function animate(time) {
   console.log(time);
   box.rotation.y = time / 1000;
