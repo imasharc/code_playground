674f78e4f998c5eec8476f1eab64bdcbe74d932b
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-07-11 10:49:24 +0200

    Making the ball/sphere bounce
    Actually the 'bouncing point' is at the sphere center when it should be at the outer surface of it, but it is fine for now

--- javascript/three_js/main.js
+++ javascript/three_js/main.js
@@ -1,5 +1,6 @@
 ﻿import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import * as dat from "dat.gui";
 
 // tool that three.js uses to allocate a space on the webpage where we can further add and animate all objects
 const renderer = new THREE.WebGLRenderer();
@@ -56,13 +57,39 @@ const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(box);
 
+// dat.GUI controls
+const gui = new dat.GUI();
+const options = {
+  sphereColor: "#ffea00",
+  wireframe: true,
+};
+// options to change the color of the sphere
+gui.addColor(options, "sphereColor").onChange(function (e) {
+  sphere.material.color.set(e);
+});
+// options to change the display of the wireframe of the sphere
+gui.add(options, "wireframe").onChange(function (e) {
+  sphere.material.wireframe = e;
+});
+
+// attributes for making the sphere/ball bounce
+let step = 0;
+let speed = 0.01;
+
 // continuously update the rotation of the box object based on the current time (timestamp)
 // By dividing the time by 1000, it converts the time from milliseconds to seconds
 // The resulting value is then assigned to the rotation.y and rotation.x properties of the box, causing it to rotate over time.
 function animate(time) {
   console.log(time);
+
+  // rotating the box
   box.rotation.y = time / 1000;
   box.rotation.x = time / 1000;
+
+  // bouncing the sphere/ball
+  step += speed;
+  sphere.position.y = 10 * Math.abs(Math.sin(step));
+
   renderer.render(scene, camera);
 }
 
